81ad92610490e0efdd1083532ca90cc045e8870e
修家絃喜 <genki561907@icloud.com>
2025-05-24 16:24:20 +0900

    getメソッドを4つ追加

--- src/main/java/org/example/model/PinionGear.java
+++ src/main/java/org/example/model/PinionGear.java
@@ -1,7 +1,7 @@
 package org.example.model;
 
 import java.awt.Color;
-
+import java.awt.Point;
 import java.awt.geom.Point2D;
 
 public class PinionGear extends SpiroGear {
@@ -35,5 +35,21 @@ public class PinionGear extends SpiroGear {
 
     public void changeSpeed(double speed) {
         this.speed = speed;
+    }
+
+    public Point2D.Double getPinionPosition() {
+        return position;
+    }
+
+    public double getPinionRadius() {
+        return radius;
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+
+    public double getAlpha() {
+        return alpha;
     }
 }
