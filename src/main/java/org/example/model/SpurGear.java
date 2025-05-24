ef81ee342f68eb991d6739dc535e6a778a0c8f26
修家絃喜 <genki561907@icloud.com>
2025-05-24 16:11:48 +0900

    スパーギアの座標を返すメソッドを追加

--- src/main/java/org/example/model/SpurGear.java
+++ src/main/java/org/example/model/SpurGear.java
@@ -7,4 +7,12 @@ public class SpurGear extends SpiroGear {
     public SpurGear(Point2D.Double position, double radius, Color color) {
         super(position, radius, color);
     }
+
+    public double getSpurRadius() {
+        return radius;
+    }
+
+    public Point2D.Double getSpurPosition() {
+        return position;
+    }
 }
