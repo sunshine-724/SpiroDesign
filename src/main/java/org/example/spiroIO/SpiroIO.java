a2c528cbbf4eda2355929114c8bc72bbaee29212
aihara4041 <g2353018@cc.kyoto-su.ac.jp>
2025-05-30 16:47:12 +0900

    引き数、戻り値の追加

--- src/main/java/org/example/spiroIO/SpiroIO.java
+++ src/main/java/org/example/spiroIO/SpiroIO.java
@@ -1,47 +1,46 @@
-package G1.SpiroDesign.src.main.java.org.example.spiroIO;
+package org.example.spiroIO;
 
-
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.net.PortUnreachableException;
+
+import javax.print.attribute.standard.PresentationDirection;
+
 import java.io.IOException;
 
-import javax.swing.JFileChooser;
-
-import java.util.List;
+import java.awt.geom.Point2D;
 
 public class SpiroIO {
 
     private Model model;
+    private Pen pen;
 
-    public SpiroIO(Model model) {
+    public SpiroIO(Model model, Pen pen) {
         this.model = model;
+        this.pen = pen;
     }
 
-    public void saveSpiro(File file) {
-        SpurGear spurGear = model.getSpurGear();
-        PinionGear pinionGear = model.getPinionGear();
-        Pen pen = model.getPen();
-        List<Point> locus = model.getLocus();
-        long time = model.getTime();
-
-        SpiroData data = new SpiroData(spurGear, pinionGear, pen, locus, time);
-
+    public void saveSpiro(File file, Model model, Pen pen) {
         try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(file))) {
-            out.writeObject(data);
+            out.writeObject(model);
+            out.writeObject(pen);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public void loadSpiro(File file) {
+    public Model loadSpiro(File file) {
         try (ObjectInputStream in = new ObjectInputStream(new FileInputStream(file))) {
-            SpiroData data = (SpiroData) in.readObject();
-            model.setFromSpiroData(data);
+            Model modelData = (Model) in.readObject();
+            Pen penData = (Pen) in.readObject();
+
+            model.setFromSpiroData(modelData);
+            pen.setFromSpiroData(penData);
         } catch (IOException | ClassNotFoundException e) {
             e.printStackTrace();
         }
+        return model;
     }
 }
