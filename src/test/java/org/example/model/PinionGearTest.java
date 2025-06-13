86935a581312b9de4e01f6e8afdb3348e038983c
修家絃喜 <genki561907@icloud.com>
2025-06-13 16:00:19 +0900

    testコード修正

--- src/test/java/org/example/model/PinionGearTest.java
+++ src/test/java/org/example/model/PinionGearTest.java
@@ -10,55 +10,20 @@ public class PinionGearTest {
 
     @Test
     void testDefaultConstructor() {
-        // 入力値
-        Point2D.Double expectedPosition = new Point2D.Double(470, 400);
-        double expectedRadius = 30.0;
-
-        // テスト実行
         PinionGear gear = new PinionGear();
-        Point2D.Double actualPosition = gear.getPinionPosition();
-        double actualRadius = gear.getPinionRadius();
-
-        // 判定と出力
-        if (expectedPosition.equals(actualPosition) && Math.abs(expectedRadius - actualRadius) < 1e-9) {
-            System.out.println("良好");
-        } else {
-            System.out.println("不良");
-        }
-
-        // 従来のアサーションも残す場合
-        assertEquals(expectedPosition, actualPosition);
-        assertEquals(expectedRadius, actualRadius, 1e-9);
+        assertEquals(new Point2D.Double(470, 400), gear.getPinionPosition());
+        assertEquals(30.0, gear.getPinionRadius(), 1e-9);
     }
 
     @Test
     void testParameterizedConstructor() {
-        // 入力値と予想値
         Pen pen = new Pen(2.0, Color.GREEN, new Point2D.Double(10, 10));
         double speed = 1.5;
         double theta = Math.PI / 4;
         double alpha = 0.1;
-        Point2D.Double expectedPosition = pen.getPosition();
-        double expectedRadius = pen.penSize;
-
-        // テスト実行
         PinionGear gear = new PinionGear(pen, speed, theta, alpha);
-
-        // 判定と出力
-        if (expectedPosition.equals(gear.getPinionPosition())
-                && Math.abs(expectedRadius - gear.getPinionRadius()) < 1e-9
-                && Math.abs(speed - gear.speed) < 1e-9
-                && Math.abs(theta - gear.theta) < 1e-9
-                && Math.abs(alpha - gear.alpha) < 1e-9
-                && pen.equals(gear.getPen())) {
-            System.out.println("良好");
-        } else {
-            System.out.println("不良");
-        }
-
-        // アサーション
-        assertEquals(expectedPosition, gear.getPinionPosition());
-        assertEquals(expectedRadius, gear.getPinionRadius(), 1e-9);
+        assertEquals(pen.getPosition(), gear.getPinionPosition());
+        assertEquals(pen.penSize, gear.getPinionRadius(), 1e-9);
         assertEquals(speed, gear.speed, 1e-9);
         assertEquals(theta, gear.theta, 1e-9);
         assertEquals(alpha, gear.alpha, 1e-9);
@@ -67,97 +32,41 @@ public class PinionGearTest {
 
     @Test
     void testMove() {
-        // 入力値と予想値
         Pen pen = new Pen(2.0, Color.BLUE, new Point2D.Double(0, 0));
         PinionGear gear = new PinionGear(pen, 0.01, 0.0, 0.0);
         Point2D.Double spurPos = new Point2D.Double(100, 100);
         double spurRadius = 50.0;
+        gear.move(100, spurRadius, spurPos);
         double expectedTheta = 0.01 * 100;
         double expectedX = 100 + (spurRadius - gear.getPinionRadius()) * Math.cos(-expectedTheta);
         double expectedY = 100 + (spurRadius - gear.getPinionRadius()) * Math.sin(-expectedTheta);
-        Point2D.Double expectedPosition = new Point2D.Double(expectedX, expectedY);
-
-        // テスト実行
-        gear.move(100, spurRadius, spurPos);
-
-        // 判定と出力
-        if (Math.abs(expectedTheta - gear.getTheta()) < 1e-9
-                && expectedPosition.equals(gear.getPinionPosition())) {
-            System.out.println("良好");
-        } else {
-            System.out.println("不良");
-        }
-
-        // アサーション
         assertEquals(expectedTheta, gear.getTheta(), 1e-9);
-        assertEquals(expectedPosition, gear.getPinionPosition());
+        assertEquals(new Point2D.Double(expectedX, expectedY), gear.getPinionPosition());
     }
 
     @Test
     void testSetPosition() {
-        // 入力値と予想値
         PinionGear gear = new PinionGear();
         Point2D.Double pos = new Point2D.Double(7, 8);
-
-        // テスト実行
         gear.setPosition(pos);
-
-        // 判定と出力
-        if (pos.equals(gear.getPinionPosition())) {
-            System.out.println("良好");
-        } else {
-            System.out.println("不良");
-        }
-
-        // アサーション
         assertEquals(pos, gear.getPinionPosition());
     }
 
     @Test
     void testChangeSpeed() {
-        // 入力値と予想値
         PinionGear gear = new PinionGear();
-        double expectedSpeed = 2.2;
-
-        // テスト実行
-        gear.changeSpeed(expectedSpeed);
-
-        // 判定と出力
-        if (Math.abs(expectedSpeed - gear.speed) < 1e-9) {
-            System.out.println("良好");
-        } else {
-            System.out.println("不良");
-        }
-
-        // アサーション
-        assertEquals(expectedSpeed, gear.speed, 1e-9);
+        gear.changeSpeed(2.2);
+        assertEquals(2.2, gear.speed, 1e-9);
     }
 
     @Test
     void testGetters() {
-        // 入力値と予想値
         Pen pen = new Pen(1.0, Color.BLACK, new Point2D.Double(0, 0));
-        double speed = 3.3;
-        double theta = 4.4;
-        double alpha = 5.5;
-
-        // テスト実行
-        PinionGear gear = new PinionGear(pen, speed, theta, alpha);
-
-        // 判定と出力
-        if (Math.abs(speed - gear.speed) < 1e-9
-                && Math.abs(theta - gear.getTheta()) < 1e-9
-                && Math.abs(alpha - gear.getAlpha()) < 1e-9
-                && pen.equals(gear.getPen())) {
-            System.out.println("良好");
-        } else {
-            System.out.println("不良");
-        }
-
-        // アサーション
-        assertEquals(speed, gear.speed, 1e-9);
-        assertEquals(theta, gear.getTheta(), 1e-9);
-        assertEquals(alpha, gear.getAlpha(), 1e-9);
+        PinionGear gear = new PinionGear(pen, 3.3, 4.4, 5.5);
+        assertEquals(3.3, gear.speed, 1e-9);
+        assertEquals(4.4, gear.getTheta(), 1e-9);
+        assertEquals(5.5, gear.getAlpha(), 1e-9);
         assertEquals(pen, gear.getPen());
     }
 }
+
--- src/test/java/org/example/model/SpiroGearTest.java
+++ src/test/java/org/example/model/SpiroGearTest.java
@@ -11,70 +11,29 @@ public class SpiroGearTest {
 
     @Test
     void testConstructor() {
-        // 入力値と予想される値を先に決める
-        Point2D.Double expectedPos = new Point2D.Double(100, 200);
-        double expectedRadius = 50.0;
-        Color expectedColor = Color.RED;
+        Point2D.Double pos = new Point2D.Double(100, 200);
+        double radius = 50.0;
+        Color color = Color.RED;
 
-        // テスト対象のインスタンス生成
-        SpiroGear gear = new SpiroGear(expectedPos, expectedRadius, expectedColor);
+        SpiroGear gear = new SpiroGear(pos, radius, color);
 
-        // 予想通りなら「良好」と出力
-        boolean result = expectedPos.equals(gear.position)
-                && Math.abs(expectedRadius - gear.radius) < 1e-9
-                && expectedColor.equals(gear.color);
-
-        if (result) {
-            System.out.println("良好");
-        } else {
-            System.out.println("不良");
-        }
-
-        // 通常のアサーションも残しておく
-        assertEquals(expectedPos, gear.position);
-        assertEquals(expectedRadius, gear.radius, 1e-9);
-        assertEquals(expectedColor, gear.color);
+        assertEquals(pos, gear.position);
+        assertEquals(radius, gear.radius, 1e-9);
+        assertEquals(color, gear.color);
     }
 
     @Test
     void testChangeRadius() {
-        // 入力値と予想される値を先に決める
-        double initialRadius = 30.0;
-        double newRadius = 45.5;
-        SpiroGear gear = new SpiroGear(new Point2D.Double(0, 0), initialRadius, Color.BLUE);
-
-        // 半径を変更
-        gear.changeRadius(newRadius);
-
-        // 予想通りなら「良好」と出力
-        if (Math.abs(newRadius - gear.radius) < 1e-9) {
-            System.out.println("良好");
-        } else {
-            System.out.println("不良");
-        }
-
-        // 通常のアサーションも残しておく
-        assertEquals(newRadius, gear.radius, 1e-9);
+        SpiroGear gear = new SpiroGear(new Point2D.Double(0, 0), 30.0, Color.BLUE);
+        gear.changeRadius(45.5);
+        assertEquals(45.5, gear.radius, 1e-9);
     }
 
     @Test
     void testChangeColor() {
-        // 入力値と予想される値を先に決める
-        Color initialColor = Color.BLUE;
-        Color newColor = Color.GREEN;
-        SpiroGear gear = new SpiroGear(new Point2D.Double(0, 0), 30.0, initialColor);
-
-        // 色を変更
-        gear.changeColor(newColor);
-
-        // 予想通りなら「良好」と出力
-        if (newColor.equals(gear.color)) {
-            System.out.println("良好");
-        } else {
-            System.out.println("不良");
-        }
-
-        // 通常のアサーションも残しておく
-        assertEquals(newColor, gear.color);
+        SpiroGear gear = new SpiroGear(new Point2D.Double(0, 0), 30.0, Color.BLUE);
+        gear.changeColor(Color.GREEN);
+        assertEquals(Color.GREEN, gear.color);
     }
 }
+
